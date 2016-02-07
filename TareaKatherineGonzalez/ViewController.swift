9925bf42ca690e7e363e83934f128e2d4c411d05
Katherine Gonzalez <katherinegonzalez@Katherines-Mac-mini.local>
2016-02-06 20:16:36 -0600

    Initial Commit

---
+++ TareaKatherineGonzalez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareaKatherineGonzalez
+//
+//  Created by Katherine Gonzalez on 6/2/16.
+//  Copyright (c) 2016 Katherine Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
