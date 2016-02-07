c304b0d7278c1d16ec2fcd4e2ea4fe6f51631727
Katherine Gonzalez <katherinegonzalez@Katherines-Mac-mini.local>
2016-02-06 20:37:32 -0600

    playground

---
+++ tareaKGS.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+
+
+import UIKit
+
+var numeros=0...100
+
+
+for num in numeros{
+    if(num % 2==0){
+      print("# \(num)  es numero par")
+    }else{
+     print("# \(num)  es numero impar")
+    }
+    if(num % 5==0){
+        print("# \(num)  Bingo!!!")
+    }
+    if(num, >=30 && num<=40){
+        print("# \(num)  Viva Swift")
+    }
+}
+
+
